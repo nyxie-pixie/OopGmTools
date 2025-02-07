9ad4d50ec3f19942f29298cc5ca7215d31533cb3
nyxie-pixie <nyxmtws26@wellingtonjags.org>
2025-02-07 09:02:58 -0500

    Add files via upload
    Let's hope I got everything uploaded this time, I added some of the more important files just in case because I am about to leave for West Virginia for the weekend

---
+++ Class.cs
@@ -0,0 +1,31 @@
+﻿namespace GMsAssistant
+{
+	public class Character
+	{
+		public string Name { get; set; }
+		public int HPMax { get; set; }
+		public int HPCurrent { get; set; }
+		public int Initiative { get; set; }
+		public int CharID { get; set; }
+	}
+	public class Dice()
+	{
+		public int Faces { get; set; }
+		public int NumberOfDice { get; set; }
+		public int Roll(int NumberOfDice, int Faces)
+		{
+			int total = 0;
+			Random die = new Random();
+			while (NumberOfDice > 0)
+			{
+				
+				
+				int rolledDie = die.Next(1, (Faces + 1));
+				total += rolledDie;
+				NumberOfDice--;
+				
+			}
+			return total;
+		}
+	}
+}
